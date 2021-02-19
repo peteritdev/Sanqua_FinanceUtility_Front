88105695cf2e457b8bf3ecb2e51c1c335644cae5
glitchfer <arif.0496.rahman@gmail.com>
2021-02-19 11:31:28 +0700

    add ecosystem.js

---
+++ ecosystem.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    apps: [
+      {
+        name: "SanQua-Attendance-System",
+        script: "./node_modules/@vue/cli-service/bin/vue-cli-service.js",
+        args: "serve"
+      }
+    ]
+};
