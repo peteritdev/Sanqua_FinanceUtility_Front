8dc0d43d8f71c11e343aaa304cfe22d6e612928a
glitchfer <arif.0496.rahman@gmail.com>
2021-02-23 10:23:23 +0700

    change to local api

--- src/services/downloadservice.js
+++ src/services/downloadservice.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import ApiService from './apiService'
 // import authHeader from './authheader'
 
-const API_URL = ApiService.private('fautility') + '/download'
+const API_URL = ApiService.local('fautility') + '/download'
 // const API_URL = 'http://localhost:6189/api/sanqua_fautility/v1/download'
 
 class DownloadService {
