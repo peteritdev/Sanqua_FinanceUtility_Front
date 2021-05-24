90ca9138e26f4a7086f5e7a0251ca03f6d6081d0
glitchfer <arif.0496.rahman@gmail.com>
2021-05-24 11:41:22 +0700

    up

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
   env: {
-    node: true,
+    node: true
   },
   extends: 'vuetify',
   rules: {
-    // 'space-before-function-paren': 0,
+    'space-before-function-paren': 0,
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint',
-  },
+    parser: 'babel-eslint'
+  }
 }
