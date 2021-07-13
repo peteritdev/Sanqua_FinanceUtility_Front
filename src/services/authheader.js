dec308483855b7207ca5cef6c86d1b12d154d224
glitchfer <arif.0496.rahman@gmail.com>
2021-07-13 11:53:24 +0700

    up

--- src/services/authheader.js
+++ src/services/authheader.js
@@ -3,7 +3,7 @@ export default function authHeader () {
 
   if (user && user.token) {
     // for Node.js Express back-end
-    return { 'x-token': user.token, 'x-method': 'conventional' }
+    return { 'x-token': user.token, 'x-method': 'conventional', 'x-application-id': '17' }
   } else {
     return {}
   }
